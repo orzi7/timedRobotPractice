b01e5b5a44805d5a1d3b5a1e3e9c6e09cc750b58
orzi7 <140631583+orzi7@users.noreply.github.com>
2024-07-09 10:58:58 +0300

    Revert "feat(timedRobotPractice/): add two axises movement"
    This reverts commit ec5c39855fd20a88740e151f142ee200be00115b.

--- timedRobotPractice/src/main/java/frc/robot/Robot.java
+++ timedRobotPractice/src/main/java/frc/robot/Robot.java
@@ -15,8 +15,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private double leftSidePowe;
-  
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -24,7 +26,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
   }
 
   /**
@@ -49,13 +53,23 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
   }
 
   /** This function is called once when teleop is enabled. */
