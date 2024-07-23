cd0cfdd5082dda75f7981d163201c64124363027
orzi7 <140631583+orzi7@users.noreply.github.com>
2024-07-24 02:05:00 +0300

    fix(GlobalConstants/): update constants classes

---
+++ timedRobotPractice/src/main/java/frc/robot/GlobalConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+public class GlobalConstants {
+    
+    class JoystickConsistants {
+        public final static int driverJoystickPort = 0;
+
+        public final static int XButton = 1;
+        public final static int OButton = 2;
+
+        public final static int YAxis = 1;
+    }
+
+    class MotorConsistants {
+        public final static int motorId = 1;
+        
+        public final static double motorPower = 0.3;
+    }
+}
