73b2f2e05920dfb39620a6653731dd085d537f72
orzi7 <140631583+orzi7@users.noreply.github.com>
2024-07-25 00:07:52 +0300

    feat(GlobalConstants/): add and use constants file

---
+++ timedRobotPractice/src/main/java/frc/robot/GlobalConstants.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+public class GlobalConstants {
+    
+    class JoystickConstants {
+        public final static int joystickPort = 0;
+        
+        public final static int leftYAxis = 1;
+        public final static int rightYAxis = 3;
+    }
+
+    class MotorConstants {
+        public final static int firstMotorRightPort = 3;
+        public final static int secondMotorRightPort = 4;
+
+        public final static int firstMotorLeftPort = 1;
+        public final static int secondMotorLeftPort = 2;
+
+
+    }
+
+}
--- timedRobotPractice/src/main/java/frc/robot/Robot.java
+++ timedRobotPractice/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,13 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+
+import frc.robot.GlobalConstants.*;
 
 
 public class Robot extends TimedRobot {
@@ -22,21 +25,27 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    joystick = new Joystick(0);
+    joystick = new Joystick(JoystickConstants.joystickPort);
 
-    firstMotorRight = new TalonSRX(3);
-    secondMotorRight = new TalonSRX(4);
+    firstMotorRight = new TalonSRX(MotorConstants.firstMotorRightPort);
+    secondMotorRight = new TalonSRX(MotorConstants.secondMotorRightPort);
 
-    secondMotorRight.setInverted(true);    
     firstMotorRight.setInverted(true);
-
-
+    secondMotorRight.setInverted(true);
+    
     firstMotorRight.follow(secondMotorRight);
 
-    firstMotorLeft = new TalonSRX(1);
-    secondMotorLeft = new TalonSRX(2);
+    firstMotorRight.setNeutralMode(NeutralMode.Brake);
+    secondMotorRight.setNeutralMode(NeutralMode.Brake);
+
+
+    firstMotorLeft = new TalonSRX(MotorConstants.firstMotorLeftPort);
+    secondMotorLeft = new TalonSRX(MotorConstants.secondMotorLeftPort);
 
     firstMotorLeft.follow(secondMotorLeft);
+
+    firstMotorLeft.setNeutralMode(NeutralMode.Brake);
+    secondMotorLeft.setNeutralMode(NeutralMode.Brake);
   }
 
 
@@ -58,14 +67,12 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {
-
-    
-    rightSidePower = joystick.getRawAxis(3);
+  public void teleopPeriodic() {    
+    rightSidePower = joystick.getRawAxis(JoystickConstants.rightYAxis);
     secondMotorRight.set(ControlMode.PercentOutput, rightSidePower);
-    leftSidePower = joystick.getRawAxis(1);
-    secondMotorLeft.set(ControlMode.PercentOutput, leftSidePower);
 
+    leftSidePower = joystick.getRawAxis(JoystickConstants.leftYAxis);
+    secondMotorLeft.set(ControlMode.PercentOutput, leftSidePower);
   }
 
   @Override
