a2c52b67968ad88cbc86677ee99d9b95236e1772
orzi7 <140631583+orzi7@users.noreply.github.com>
2024-07-27 18:50:12 +0300

    feat(Robot/): add RT and LT control and constant file and use

---
+++ timedRobotPractice/src/main/java/frc/robot/GlobalConstants.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+public class GlobalConstants {
+    
+    class JoystickConstants {
+        public final static int joystickPort = 0;
+        
+        public final static int YAxis = 1;
+        public final static int XAxis = 2;
+
+        public final static int RTButton = 5;
+        public final static int LTButton = 4;
+    }
+
+    class MotorConstants {
+        public final static int firstMotorRightPort = 3;
+        public final static int secondMotorRightPort = 4;
+
+        public final static int firstMotorLeftPort = 1;
+        public final static int secondMotorLeftPort = 2;
+    }
+
+}
--- timedRobotPractice/src/main/java/frc/robot/Robot.java
+++ timedRobotPractice/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,12 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.GlobalConstants.*;
 
 
 public class Robot extends TimedRobot {
@@ -12,7 +14,7 @@ public class Robot extends TimedRobot {
   private double towardSpeed;
   private double turnSpeed;
 
-  private Joystick joystick;
+  private Joystick driverJoystick;
 
   private TalonSRX firstMotorRight;
   private TalonSRX secondMotorRight;
@@ -22,21 +24,27 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    joystick = new Joystick(0);
+    driverJoystick = new Joystick(JoystickConstants.joystickPort);
 
-    firstMotorRight = new TalonSRX(3);
-    secondMotorRight = new TalonSRX(4);
-
-    secondMotorRight.setInverted(true);    
-    firstMotorRight.setInverted(true);
-
+    firstMotorRight = new TalonSRX(MotorConstants.firstMotorRightPort);
+    secondMotorRight = new TalonSRX(MotorConstants.secondMotorRightPort);
 
     firstMotorRight.follow(secondMotorRight);
+    
+    firstMotorRight.setNeutralMode(NeutralMode.Brake);
+    secondMotorRight.setNeutralMode(NeutralMode.Brake);
+    
+    
+    firstMotorLeft = new TalonSRX(MotorConstants.firstMotorLeftPort);
+    secondMotorLeft = new TalonSRX(MotorConstants.secondMotorLeftPort);
 
-    firstMotorLeft = new TalonSRX(1);
-    secondMotorLeft = new TalonSRX(2);
+    firstMotorLeft.setInverted(true);
+    secondMotorLeft.setInverted(true);
 
     firstMotorLeft.follow(secondMotorLeft);
+
+    firstMotorLeft.setNeutralMode(NeutralMode.Brake);
+    secondMotorLeft.setNeutralMode(NeutralMode.Brake);
   }
 
 
@@ -60,20 +68,36 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    towardSpeed = joystick.getRawAxis(3);    
-    turnSpeed = joystick.getRawAxis(2);
+    towardSpeed = driverJoystick.getRawAxis(JoystickConstants.YAxis);    
+    turnSpeed = driverJoystick.getRawAxis(JoystickConstants.XAxis);
 
-    if (Math.abs(turnSpeed) > 0.05) {
-      secondMotorRight.set(ControlMode.PercentOutput, -turnSpeed);
-      secondMotorLeft.set(ControlMode.PercentOutput, turnSpeed);
+
+    if (Math.abs(turnSpeed) > 0.05 || Math.abs(towardSpeed) > 0.05) {
+      if (Math.abs(turnSpeed) > 0.05) {
+        secondMotorRight.set(ControlMode.PercentOutput, turnSpeed);
+        secondMotorLeft.set(ControlMode.PercentOutput, -turnSpeed);
+      }
+      else if (Math.abs(towardSpeed) > 0.05) {
+        secondMotorRight.set(ControlMode.PercentOutput, towardSpeed);
+        secondMotorLeft.set(ControlMode.PercentOutput, towardSpeed);
+      }
+      else {
+        secondMotorRight.set(ControlMode.PercentOutput, 0);
+        secondMotorLeft.set(ControlMode.PercentOutput, 0);
+      }
     }
-
-    else if (Math.abs(towardSpeed) > 0.05) {
-      secondMotorRight.set(ControlMode.PercentOutput, towardSpeed);
-      secondMotorLeft.set(ControlMode.PercentOutput, towardSpeed);
+    else if (driverJoystick.getRawButton(JoystickConstants.RTButton) || driverJoystick.getRawButton(JoystickConstants.LTButton)) {
+      if (driverJoystick.getRawButton(JoystickConstants.RTButton)) {
+        secondMotorRight.set(ControlMode.PercentOutput, towardSpeed);
+      }
+      else if (driverJoystick.getRawButton(JoystickConstants.LTButton)) {
+        secondMotorLeft.set(ControlMode.PercentOutput, towardSpeed);
+      }
+      else {
+        secondMotorRight.set(ControlMode.PercentOutput, 0);
+        secondMotorLeft.set(ControlMode.PercentOutput, 0);
+      }
     }
-
-
   }
 
   @Override
